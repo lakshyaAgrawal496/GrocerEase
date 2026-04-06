bd0877187a8a599de99a0f9d15ff41c7742a2b93
Lakshya Agrawal <agrawallakshyaaa@gmail.com>
2026-04-06 16:43:52 +0530

    feat: configure Vite build with manual vendor chunk splitting

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
